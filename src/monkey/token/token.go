bae9dfca8062a6490f673274b6903932840f1bd3
shinshu68 <shinshu68@gmail.com>
2019-04-20 17:37:22 +0900

    1.4 トークン集合の拡充と字句解析器の拡張

--- src/monkey/token/token.go
+++ src/monkey/token/token.go
@@ -16,6 +16,16 @@ const (
     // 演算子
     ASSIGN   = "="
     PLUS     = "+"
+    MINUS    = "-"
+    BANG     = "!"
+    ASTERISK = "*"
+    SLASH    = "/"
+
+    LT = "<"
+    GT = ">"
+
+    EQ     = "=="
+    NOT_EQ = "!="
 
     // デリミタ
     COMMA     = ","
@@ -29,11 +39,21 @@ const (
     // キーワード
     FUNCTION = "FUNCTION"
     LET      = "LET"
+    TRUE     = "TRUE"
+    FALSE    = "FALSE"
+    IF       = "IF"
+    ELSE     = "ELSE"
+    RETURN   = "RETURN"
 )
 
 var keywords = map[string]TokenType{
     "fn":     FUNCTION,
     "let":    LET,
+    "true":   TRUE,
+    "false":  FALSE,
+    "if":     IF,
+    "else":   ELSE,
+    "return": RETURN,
 }
 
 func LookupIdent(ident string) TokenType {

26ae04b4555b912e3c04ad0a49cc6ed285aa313e
shinshu68 <shinshu68@gmail.com>
2019-04-21 13:57:55 +0900

    2.6.5 Pratt構文解析器の実装

--- src/monkey/parser/parser.go
+++ src/monkey/parser/parser.go
@@ -13,6 +13,14 @@ type Parser struct {
     curToken  token.Token
     peekToken token.Token
     errors    []string
+
+    prefixParseFns map[token.TokenType]prefixParseFn
+    infixParseFns  map[token.TokenType]infixParseFn
+}
+
+type {
+    prefixParseFn func() ast.Expression
+    infixParseFn func(ast.Expression) ast.Expression
 }
 
 func New(l *lexer.Lexer) *Parser {
@@ -120,4 +128,12 @@ func (p *Parser) peekError(t token.TokenType) {
     msg := fmt.Sprintf("expected next token to be %s, got %s instead",
         t, p.peekToken.Type)
     p.errors = append(p.errors, msg)
+}
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+    p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+    p.infixParseFns[tokenType] = fn
 }

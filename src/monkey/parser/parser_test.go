93b4d8dd886a3c4df54aa15abad66d00d27982c7
shinshu68 <shinshu68@gmail.com>
2019-04-29 20:50:24 +0900

    2.8 構文解析器の拡張

--- src/monkey/parser/parser_test.go
+++ src/monkey/parser/parser_test.go
@@ -253,6 +253,22 @@ func TestParsinginfixExpressions(t *testing.T) {
     }
 }
 
+func testIdentifier(t *testing.T, exp ast.Expression, value string) bool {
+    ident, ok := exp.(*ast.Identifier)
+    if !ok {
+        t.Errorf("exp not *ast.Identifier. got=%T", exp)
+        return false
+    }
+
+    if ident.Value != value {
+        t.Errorf("ident.Value not %s. got=%s", value,
+            ident.TokenLiteral())
+        return false
+    }
+
+    return true
+}
+
 func TestOperatorPrecedenceParsing(t *testing.T) {
     tests := []struct {
         input    string
@@ -319,4 +335,46 @@ func TestOperatorPrecedenceParsing(t *testing.T) {
             t.Errorf("expected=%q, got=%q", tt.expected, actual)
         }
     }
+}
+
+func testLiteralExpression(
+    t *testing.T,
+    exp ast.Expression,
+    expected interface(),
+) bool {
+    switch v:= expected.(type) {
+        case int:
+            return testIntegerLiteral(t, exp, int64(v))
+        case int64:
+            return testIntegerLiteral(t, exp, v)
+        case string:
+            return testIdentifier(t, exp, v)
+    }
+    t.Errorf("type of exp not handled. got=%T", exp)
+    return false
+}
+
+func testInfixExpression(t *testing.T, exp ast.Expression, left interface().
+    operator string, right interface()) bool {
+
+    onExp, ok := exp.(*ast.InfixExpression)
+    if !ok {
+        t.Errorf("exp is not ast.InfixExpression. got=%T(%s)", exp, exp)
+        return false
+    }
+
+    if !testLiteralExpression(t, onExp, left) {
+        return false
+    }
+
+    if onExp.Operator != operator {
+        t.Errorf("exp.Operator is not '%s'. got=%q", operator, onExp.Operator)
+        return false
+    }
+
+    if !testLiteralExpression(t, onExp.Right, right) {
+        return false
+    }
+
+    return true
 }
